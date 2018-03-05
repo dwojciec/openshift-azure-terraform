87cad141a7aba2f751d861bc17486a32a4c037fb
dwojciec <dwojciec@users.noreply.github.com>
2018-03-05 15:28:31 +0100

    add azure param

--- openshift-enterprise/scripts/deployOpenShift.sh
+++ openshift-enterprise/scripts/deployOpenShift.sh
@@ -21,16 +21,18 @@ OCP=${13}
 BASTION=$(hostname -f)
 REGISTRY_STORAGE_ACCOUNT_NAME=${14}
 PRIMARY_ACCESS_KEY=${15}
-TENANT_ID=${16}
-SUBSCRIPTION_ID=${17}
-AAD_CLIENT_ID=${18}
-ACNT_SECRET=${19}
-RG_NAME=${20}
-RG_LOCATION=${21}
+
+TENANTID=${16}
+SUBSCRIPTIONID=${17}
+AADCLIENTID=${18}
+AADCLIENTSECRET=${19}
+RESOURCEGROUP=${20}
+LOCATION=${21}
 
 
 MASTERLOOP=$((MASTERCOUNT - 1))
 NODELOOP=$((NODECOUNT - 1))
+INFRALOOP=$((INFRACOUNT -1))
 
 DOMAIN=$( awk 'NR==2' /etc/resolv.conf | awk '{ print $2 }' )
 echo " Print variable entry :"
@@ -50,12 +52,12 @@ echo " 13 - OCP :" $OCP
 
 echo "14 - REGISTRY_STORAGE_ACCOUNT_NAME:" $REGISTRY_STORAGE_ACCOUNT_NAME
 echo "15 - PRIMARY_ACCESS_KEY:" $PRIMARY_ACCESS_KEY
-echo "16 - TENANT_ID:" $TENANT_ID 
-echo "17 - SUBSCRIPTION_ID: "$SUBSCRIPTION_ID
-echo "18 - AAD_CLIENT_ID:" $AAD_CLIENT_ID
-echo "19 - ACNT_SECRET:" $ACNT_SECRET
-echo "20 - RG_NAME:" $RG_NAME
-echo "21 - RG_LOCATION:" $RG_LOCATION
+echo "16 - TENANTID:" $TENANTID 
+echo "17 - SUBSCRIPTIONID: "$SUBSCRIPTIONID
+echo "18 - AADCLIENTID:" $AADCLIENTID
+echo "19 - AADCLIENTSECRET:" $AADCLIENTSECRET
+echo "20 - RESOURCEGROUP:" $RESOURCEGROUP
+echo "21 - LOCATION:" $LOCATION
 
 # Generate private keys for use by Ansible
 echo $(date) " - Generating Private keys for use by Ansible for OpenShift Installation"
@@ -82,7 +84,7 @@ cat > /home/${SUDOUSER}/preinstall.yml <<EOF
   become: yes
   become_method: sudo
   vars:
-    description: "Create OpenShift Users"
+    description: "Create /etc/hosts file"
   tasks:
   - name: copy hosts file
     copy:
@@ -137,6 +139,317 @@ cat > /home/${SUDOUSER}/postinstall3.yml <<EOF
     shell: echo "${PASSWORD}"|passwd root --stdin
 EOF
 
+# Run on MASTER-0 - configure registry to use Azure Storage
+cat > /home/${SUDOUSER}/dockerregistry.yml <<EOF
+---
+- hosts: master0
+  gather_facts: no
+  remote_user: ${SUDOUSER}
+  become: yes
+  become_method: sudo
+  vars:
+    description: "Set registry to use Azure Storage"
+  tasks:
+  - name: Configure docker-registry to use Azure Storage
+    shell: oc env dc docker-registry -e REGISTRY_STORAGE=azure -e REGISTRY_STORAGE_AZURE_ACCOUNTNAME=$REGISTRYSA -e REGISTRY_STORAGE_AZURE_ACCOUNTKEY=$ACCOUNTKEY -e REGISTRY_STORAGE_AZURE_CONTAINER=registry
+EOF
+
+# Run on MASTER-0 - configure Storage Class
+
+cat > /home/${SUDOUSER}/configurestorageclass.yml <<EOF
+---
+- hosts: master0
+  gather_facts: no
+  remote_user: ${SUDOUSER}
+  become: yes
+  become_method: sudo
+  vars:
+    description: "Create Storage Class"
+  tasks:
+  - name: Create Storage Class with StorageAccountPV1
+    shell: oc create -f /home/${SUDOUSER}/scgeneric1.yml
+EOF
+
+# Create vars.yml file for use by setup-azure-config.yml playbook
+
+cat > /home/${SUDOUSER}/vars.yml <<EOF
+g_tenantId: $TENANTID
+g_subscriptionId: $SUBSCRIPTIONID
+g_aadClientId: $AADCLIENTID
+g_aadClientSecret: $AADCLIENTSECRET
+g_resourceGroup: $RESOURCEGROUP
+g_location: $LOCATION
+EOF
+
+# Create Azure Cloud Provider configuration Playbook for Master Config
+
+if [ $MASTERCOUNT -eq 1 ]
+then
+
+# Single Master Configuration
+
+cat > /home/${SUDOUSER}/setup-azure-master.yml <<EOF
+#!/usr/bin/ansible-playbook
+- hosts: masters
+  gather_facts: no
+  serial: 1
+  vars_files:
+  - vars.yml
+  become: yes
+  vars:
+    azure_conf_dir: /etc/azure
+    azure_conf: "{{ azure_conf_dir }}/azure.conf"
+    master_conf: /etc/origin/master/master-config.yaml
+  handlers:
+  - name: restart origin-master-api
+    systemd:
+      state: restarted
+      name: origin-master-api
+  - name: restart origin-master-controllers
+    systemd:
+      state: restarted
+      name: origin-master-controllers
+  post_tasks:
+  - name: make sure /etc/azure exists
+    file:
+      state: directory
+      path: "{{ azure_conf_dir }}"
+  - name: populate /etc/azure/azure.conf
+    copy:
+      dest: "{{ azure_conf }}"
+      content: |
+        {
+          "aadClientID" : "{{ g_aadClientId }}",
+          "aadClientSecret" : "{{ g_aadClientSecret }}",
+          "subscriptionID" : "{{ g_subscriptionId }}",
+          "tenantID" : "{{ g_tenantId }}",
+          "resourceGroup": "{{ g_resourceGroup }}",
+        }
+    notify:
+    - restart origin-master-api
+    - restart origin-master-controllers
+  - name: insert the azure disk config into the master
+    modify_yaml:
+      dest: "{{ master_conf }}"
+      yaml_key: "{{ item.key }}"
+      yaml_value: "{{ item.value }}"
+    with_items:
+    - key: kubernetesMasterConfig.apiServerArguments.cloud-config
+      value:
+      - "{{ azure_conf }}"
+    - key: kubernetesMasterConfig.apiServerArguments.cloud-provider
+      value:
+      - azure
+    - key: kubernetesMasterConfig.controllerArguments.cloud-config
+      value:
+      - "{{ azure_conf }}"
+    - key: kubernetesMasterConfig.controllerArguments.cloud-provider
+      value:
+      - azure
+    notify:
+    - restart origin-master-api
+    - restart origin-master-controllers
+EOF
+
+else
+
+# Multiple Master Configuration
+
+cat > /home/${SUDOUSER}/setup-azure-master.yml <<EOF
+#!/usr/bin/ansible-playbook
+- hosts: masters
+  gather_facts: no
+  serial: 1
+  vars_files:
+  - vars.yml
+  become: yes
+  vars:
+    azure_conf_dir: /etc/azure
+    azure_conf: "{{ azure_conf_dir }}/azure.conf"
+    master_conf: /etc/origin/master/master-config.yaml
+  handlers:
+  - name: restart origin-master-api
+    systemd:
+      state: restarted
+      name: origin-master-api
+  - name: restart origin-master-controllers
+    systemd:
+      state: restarted
+      name: origin-master-controllers
+  post_tasks:
+  - name: make sure /etc/azure exists
+    file:
+      state: directory
+      path: "{{ azure_conf_dir }}"
+  - name: populate /etc/azure/azure.conf
+    copy:
+      dest: "{{ azure_conf }}"
+      content: |
+        {
+          "aadClientID" : "{{ g_aadClientId }}",
+          "aadClientSecret" : "{{ g_aadClientSecret }}",
+          "subscriptionID" : "{{ g_subscriptionId }}",
+          "tenantID" : "{{ g_tenantId }}",
+          "resourceGroup": "{{ g_resourceGroup }}",
+        }
+    notify:
+    - restart origin-master-api
+    - restart origin-master-controllers
+  - name: insert the azure disk config into the master
+    modify_yaml:
+      dest: "{{ master_conf }}"
+      yaml_key: "{{ item.key }}"
+      yaml_value: "{{ item.value }}"
+    with_items:
+    - key: kubernetesMasterConfig.apiServerArguments.cloud-config
+      value:
+      - "{{ azure_conf }}"
+    - key: kubernetesMasterConfig.apiServerArguments.cloud-provider
+      value:
+      - azure
+    - key: kubernetesMasterConfig.controllerArguments.cloud-config
+      value:
+      - "{{ azure_conf }}"
+    - key: kubernetesMasterConfig.controllerArguments.cloud-provider
+      value:
+      - azure
+    notify:
+    - restart origin-master-api
+    - restart origin-master-controllers
+EOF
+
+fi
+
+# Create Azure Cloud Provider configuration Playbook for Node Config (Master Nodes)
+
+cat > /home/${SUDOUSER}/setup-azure-node-master.yml <<EOF
+#!/usr/bin/ansible-playbook
+- hosts: masters
+  serial: 1
+  gather_facts: no
+  vars_files:
+  - vars.yml
+  become: yes
+  vars:
+    azure_conf_dir: /etc/azure
+    azure_conf: "{{ azure_conf_dir }}/azure.conf"
+    node_conf: /etc/origin/node/node-config.yaml
+  handlers:
+  - name: restart origin-node
+    systemd:
+      state: restarted
+      name: origin-node
+  post_tasks:
+  - name: make sure /etc/azure exists
+    file:
+      state: directory
+      path: "{{ azure_conf_dir }}"
+  - name: populate /etc/azure/azure.conf
+    copy:
+      dest: "{{ azure_conf }}"
+      content: |
+        {
+          "aadClientID" : "{{ g_aadClientId }}",
+          "aadClientSecret" : "{{ g_aadClientSecret }}",
+          "subscriptionID" : "{{ g_subscriptionId }}",
+          "tenantID" : "{{ g_tenantId }}",
+          "resourceGroup": "{{ g_resourceGroup }}",
+        }
+    notify:
+    - restart origin-node
+  - name: insert the azure disk config into the node
+    modify_yaml:
+      dest: "{{ node_conf }}"
+      yaml_key: "{{ item.key }}"
+      yaml_value: "{{ item.value }}"
+    with_items:
+    - key: kubeletArguments.cloud-config
+      value:
+      - "{{ azure_conf }}"
+    - key: kubeletArguments.cloud-provider
+      value:
+      - azure
+    notify:
+    - restart origin-node
+EOF
+
+# Create Azure Cloud Provider configuration Playbook for Node Config (Non-Master Nodes)
+
+cat > /home/${SUDOUSER}/setup-azure-node.yml <<EOF
+#!/usr/bin/ansible-playbook
+- hosts: nodes:!masters
+  serial: 1
+  gather_facts: no
+  vars_files:
+  - vars.yml
+  become: yes
+  vars:
+    azure_conf_dir: /etc/azure
+    azure_conf: "{{ azure_conf_dir }}/azure.conf"
+    node_conf: /etc/origin/node/node-config.yaml
+  handlers:
+  - name: restart origin-node
+    systemd:
+      state: restarted
+      name: origin-node
+  post_tasks:
+  - name: make sure /etc/azure exists
+    file:
+      state: directory
+      path: "{{ azure_conf_dir }}"
+  - name: populate /etc/azure/azure.conf
+    copy:
+      dest: "{{ azure_conf }}"
+      content: |
+        {
+          "aadClientID" : "{{ g_aadClientId }}",
+          "aadClientSecret" : "{{ g_aadClientSecret }}",
+          "subscriptionID" : "{{ g_subscriptionId }}",
+          "tenantID" : "{{ g_tenantId }}",
+          "resourceGroup": "{{ g_resourceGroup }}",
+        }
+    notify:
+    - restart origin-node
+  - name: insert the azure disk config into the node
+    modify_yaml:
+      dest: "{{ node_conf }}"
+      yaml_key: "{{ item.key }}"
+      yaml_value: "{{ item.value }}"
+    with_items:
+    - key: kubeletArguments.cloud-config
+      value:
+      - "{{ azure_conf }}"
+    - key: kubeletArguments.cloud-provider
+      value:
+      - azure
+    notify:
+    - restart origin-node
+  - name: delete the node so it can recreate itself
+    command: oc delete node {{inventory_hostname}}
+    delegate_to: ${MASTER}-0
+  - name: sleep to let node come back to life
+    pause:
+       seconds: 90
+EOF
+
+# Create Playbook to delete stuck Master nodes and set as not schedulable
+
+cat > /home/${SUDOUSER}/deletestucknodes.yml <<EOF
+- hosts: masters
+  gather_facts: no
+  become: yes
+  vars:
+    description: "Delete stuck nodes"
+  tasks:
+  - name: Delete stuck nodes so it can recreate itself
+    command: oc delete node {{inventory_hostname}}
+    delegate_to: ${MASTER}-0
+  - name: sleep between deletes
+    pause:
+      seconds: 25
+  - name: set masters as unschedulable
+    command: oadm manage-node {{inventory_hostname}} --schedulable=false
+EOF
 
 # Run on all masters
 cat > /home/${SUDOUSER}/postinstall4.yml <<EOF
@@ -162,7 +475,7 @@ echo $(date) " - Create Ansible Hosts file"
 echo "Master count :" $MASTERCOUNT
 if [ $MASTERCOUNT -eq 1 ]
 then
-
+# Ansible Host file for Single Master Configuration
 cat > /etc/ansible/hosts <<EOF
 # Create an OSEv3 group that contains the masters and nodes groups
 [OSEv3:children]
@@ -170,6 +483,8 @@ masters
 etcd
 nodes
 nfs
+master0
+new_nodes
 
 # Set variables common for all OSEv3 hosts
 [OSEv3:vars]
@@ -240,6 +555,9 @@ openshift_hosted_etcd_storage_labels={'storage': 'etcd'}
 [masters]
 $MASTER-0.$DOMAIN
 
+[master0]
+$MASTER-0.$DOMAIN
+
 [etcd]
 $MASTER-0.$DOMAIN
 
@@ -248,12 +566,29 @@ $MASTER-0.$DOMAIN
 
 # host group for nodes
 [nodes]
-$MASTER-0.$DOMAIN openshift_node_labels="{'region': 'master', 'zone': 'default'}"
-$INFRA-0.$DOMAIN openshift_node_labels="{'region': 'infra', 'zone': 'default'}"
+$MASTER-0.$DOMAIN openshift_node_labels="{'region': 'master', 'zone': 'default'}" openshift_hostname=$MASTER-0.$DOMAIN
 EOF
-for node in $OCP-node-{0..30}; do
-	echo $(ping -c 1 $node 2>/dev/null|grep ocp|grep PING|awk '{ print $2 }') openshift_node_labels=\"{\'region\': \'nodes\', \'zone\': \'default\'}\"
-done|grep ocpn >>/etc/ansible/hosts
+
+# Loop to add Infra Nodes
+
+for (( c=0; c<$INFRACOUNT; c++ ))
+do
+  echo "$INFRA-$c openshift_node_labels=\"{'type': 'infra', 'zone': 'default'}\" openshift_hostname=$INFRA-$c" >> /etc/ansible/hosts
+done
+
+# Loop to add Nodes
+
+for (( c=0; c<$NODECOUNT; c++ ))
+do
+  echo "$NODE-$c openshift_node_labels=\"{'type': 'app', 'zone': 'default'}\" openshift_hostname=$NODE-$c" >> /etc/ansible/hosts
+done
+
+# Create new_nodes group
+
+cat >> /etc/ansible/hosts <<EOF
+# host group for adding new nodes
+[new_nodes]
+EOF
 
 else
 
@@ -265,6 +600,8 @@ nodes
 etcd
 nfs
 lb
+new_nodes
+master0
 
 # Set variables common for all OSEv3 hosts
 [OSEv3:vars]
@@ -334,54 +671,66 @@ openshift_hosted_etcd_storage_labels={'storage': 'etcd'}
 
 # host group for masters
 [masters]
-EOF
-for node in $OCP-master-{0..3}; do
-	ping -c 1 $node 2>/dev/null|grep ocp|grep PING|awk '{ print $2 }' 
-done|grep ocpm >>/etc/ansible/hosts
-
-cat >> /etc/ansible/hosts <<EOF
+$MASTER-[0:${MASTERLOOP}]
 # host group for etcd
 [etcd]
-EOF
-for node in $OCP-master-{0..3}; do
-	ping -c 1 $node 2>/dev/null|grep ocp|grep PING|awk '{ print $2 }' 
-done|grep ocpm >>/etc/ansible/hosts
-
-cat >> /etc/ansible/hosts <<EOF
+$MASTER-[0:${MASTERLOOP}]
+[master0]
+$MASTER-0
 [nfs]
 $MASTER-0.$DOMAIN
-
 [lb]
 $BASTION
 
 # host group for nodes
 [nodes]
 EOF
-for node in $OCP-master-{0..3}; do
-	echo $(ping -c 1 $node 2>/dev/null|grep ocp|grep PING|awk '{ print $2 }') openshift_node_labels=\"{\'region\': \'master\', \'zone\': \'default\'}\"
-done|grep ocpm >>/etc/ansible/hosts
-for node in $OCP-infra-{0..10}; do
-	echo $(ping -c 1 $node 2>/dev/null|grep ocp|grep PING|awk '{ print $2 }') openshift_node_labels=\"{\'region\': \'infra\', \'zone\': \'default\'}\"
-done|grep ocpi >>/etc/ansible/hosts
-for node in $OCP-node-{0..30}; do
-	echo $(ping -c 1 $node 2>/dev/null|grep ocp|grep PING|awk '{ print $2 }') openshift_node_labels=\"{\'region\': \'nodes\', \'zone\': \'default\'}\"
-done|grep ocpn >>/etc/ansible/hosts
+
+# Loop to add Masters
+
+for (( c=0; c<$MASTERCOUNT; c++ ))
+do
+  echo "$MASTER-$c openshift_node_labels=\"{'type': 'master', 'zone': 'default'}\" openshift_hostname=$MASTER-$c" >> /etc/ansible/hosts
+done
+
+# Loop to add Infra Nodes
+
+for (( c=0; c<$INFRACOUNT; c++ ))
+do
+  echo "$INFRA-$c openshift_node_labels=\"{'type': 'infra', 'zone': 'default'}\" openshift_hostname=$INFRA-$c" >> /etc/ansible/hosts
+done
+
+# Loop to add Nodes
+
+for (( c=0; c<$NODECOUNT; c++ ))
+do
+  echo "$NODE-$c openshift_node_labels=\"{'type': 'app', 'zone': 'default'}\" openshift_hostname=$NODE-$c" >> /etc/ansible/hosts
+done
+
+# Create new_nodes group
+
+cat >> /etc/ansible/hosts <<EOF
+# host group for adding new nodes
+[new_nodes]
+EOF
+
 fi
 
 # Create and distribute hosts file to all nodes, this is due to us having to use 
 (
 echo "127.0.0.1   localhost localhost.localdomain localhost4 localhost4.localdomain4"
 echo "::1         localhost localhost.localdomain localhost6 localhost6.localdomain6"
-for node in $OCP-master-0 $OCP-master-1 $OCP-master-2; do
-	ping -c 1 $node 2>/dev/null|grep ocp|grep PING|awk '{ print $3 " " $2  }'|sed -e 's/(//' -e 's/)//'i -e "s/.net/.net $node/"
+for (( c=0; c<$MASTERCOUNT; c++ ))
+do
+	ping -c 1 $MASTER-$c 2>/dev/null|grep ocp|grep PING|awk '{ print $3 " " $2  }'|sed -e 's/(//' -e 's/)//'i -e "s/.net/.net $/MASTER-$c"
 done
 
-for node in $OCP-infra-{0..5}; do
-	ping -c 1 $node 2>/dev/null|grep ocp|grep PING|awk '{ print $3 " " $2  }'|sed -e 's/(//' -e 's/)//' -e "s/.net/.net $node/"
+for (( c=0; c<$INFRACOUNT; c++ ))
+	ping -c 1 $INFRA-$c 2>/dev/null|grep ocp|grep PING|awk '{ print $3 " " $2  }'|sed -e 's/(//' -e 's/)//' -e "s/.net/.net $INFRA-$c/"
 done
 
-for node in $OCP-node-{0..30}; do
-	ping -c 1 $node 2>/dev/null|grep ocp|grep PING|awk '{ print $3 " " $2  }'|sed -e 's/(//' -e 's/)//' -e "s/.net/.net $node/"
+for (( c=0; c<$NODECOUNT; c++ ))
+	ping -c 1 $NODE-$c 2>/dev/null|grep ocp|grep PING|awk '{ print $3 " " $2  }'|sed -e 's/(//' -e 's/)//' -e "s/.net/.net $NODE-$c/"
 done
 ) >/tmp/hosts
 
