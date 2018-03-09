565d72956c08b650567197957e37aa6e18eb5821
dwojciec <dwojciec@users.noreply.github.com>
2018-03-09 09:34:10 +0100

    add ssh bastion in output

--- outputs.tf
+++ outputs.tf
@@ -2,5 +2,6 @@ output "openshift_console_url" {
   value = "https://${azurerm_public_ip.openshift_master_pip.fqdn}:8443/console"
 }
 output "bastion_ssh" {
-  value = "ssh ${var.admin_username}@${azurerm_public_ip.bastion_pip.fqdn"}
+  value = "ssh ${var.admin_username}@${azurerm_public_ip.bastion_pip.fqdn"
 }
+
